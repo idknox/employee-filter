0daa30a0a950b5dc4b347b031a69624d92a264a9
Ian Knox <knoxid@gmail.com>
2014-07-10 09:25:24 -0600

    all tests pass

--- lib/employee_filter.rb
+++ lib/employee_filter.rb
@@ -1,3 +1,18 @@
 class EmployeeFilter
 
+  def initialize(employees)
+    @employees = employees
+  end
+
+  def started_before_2006
+    @employees.select {|em| em[:start_date].year < 2006}
+  end
+
+  def all_with_start_date
+    @employees.map do |em|
+      "#{em[:first_name]} #{em[:last_name]} (#{em[:title]}) - #{em[:start_date].strftime('%-m/%-d/%Y')}"
+    end
+  end
+
+
 end
